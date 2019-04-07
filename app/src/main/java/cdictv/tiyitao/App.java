7bd547aa761acccb9139a79bdbf3590b26b6d2e5
wwclearlove <1280488753@qq.com>
2019-04-07 19:14:52 +0800

    wyc

---
+++ app/src/main/java/cdictv/tiyitao/App.java
@@ -0,0 +1,13 @@
+package cdictv.tiyitao;
+
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application{
+    public static Context INSTANCE;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        INSTANCE=this;
+    }
+}
--- app/src/main/java/cdictv/tiyitao/activity/MainActivity.java
+++ app/src/main/java/cdictv/tiyitao/activity/MainActivity.java
@@ -1,15 +1,111 @@
 package cdictv.tiyitao.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import cdictv.tiyitao.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView left_menu;
+    private TextView title;
+    private TextView zhuxiao;
+    private RelativeLayout tool_bar;
+    private ImageView top_bac;
+    private LinearLayout honglvdeng;
+    private LinearLayout fazhi;
+    private LinearLayout chuxing;
+    private LinearLayout shujufenxi;
+    private LinearLayout shenghuo;
+    private LinearLayout shenghzs;
+    private LinearLayout yijian;
+    private LinearLayout tianqi;
+    private LinearLayout yule;
+    private LinearLayout navigation_view;
+    private DrawerLayout drawer_yout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        left_menu = (ImageView) findViewById(R.id.left_menu);
+        title = (TextView) findViewById(R.id.title);
+        zhuxiao = (TextView) findViewById(R.id.zhuxiao);
+        tool_bar = (RelativeLayout) findViewById(R.id.tool_bar);
+        top_bac = (ImageView) findViewById(R.id.top_bac);
+        honglvdeng = (LinearLayout) findViewById(R.id.honglvdeng);
+        fazhi = (LinearLayout) findViewById(R.id.fazhi);
+        chuxing = (LinearLayout) findViewById(R.id.chuxing);
+        shujufenxi = (LinearLayout) findViewById(R.id.shujufenxi);
+        shenghuo = (LinearLayout) findViewById(R.id.shenghuo);
+        shenghzs = (LinearLayout) findViewById(R.id.shenghzs);
+        yijian = (LinearLayout) findViewById(R.id.yijian);
+        tianqi = (LinearLayout) findViewById(R.id.tianqi);
+        yule = (LinearLayout) findViewById(R.id.yule);
+        navigation_view = (LinearLayout) findViewById(R.id.navigation_view);
+        drawer_yout = (DrawerLayout) findViewById(R.id.drawer_yout);
+
+        left_menu.setOnClickListener(this);
+        honglvdeng.setOnClickListener(this);
+        fazhi.setOnClickListener(this);
+        chuxing.setOnClickListener(this);
+        shujufenxi.setOnClickListener(this);
+        shenghuo.setOnClickListener(this);
+        shenghzs.setOnClickListener(this);
+        yijian.setOnClickListener(this);
+        tianqi.setOnClickListener(this);
+        yule.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.left_menu:
+                //侧滑
+                if (drawer_yout.isDrawerOpen(Gravity.LEFT)) {
+                    drawer_yout.closeDrawer(Gravity.LEFT);
+                } else {
+                    drawer_yout.openDrawer(Gravity.LEFT);
+                }
+                break;
+            case R.id.honglvdeng:
+
+                break;
+            case R.id.fazhi:
+
+                break;
+            case R.id.chuxing:
+
+                break;
+            case R.id.shujufenxi:
+
+                break;
+            case R.id.shenghuo:
+
+                break;
+            case R.id.shenghzs:
+
+                break;
+            case R.id.yijian:
+
+                break;
+            case R.id.tianqi:
+
+                break;
+            case R.id.yule:
+
+                break;
+        }
     }
 }
