43c06b306148a2f89895266986c1be2e12bbbfa9
wwclearlove <1280488753@qq.com>
2019-04-07 18:42:31 +0800

    name

---
+++ app/src/main/java/cdictv/tiyitao/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package cdictv.tiyitao.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cdictv.tiyitao.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
